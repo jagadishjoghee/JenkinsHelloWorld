fa0b3d94bc1b3518e0fca2123d721b2af29ae7a1
jagadishjoghee <jaga.joghee@gmail.com>
2022-07-18 17:55:52 +0530

    Update HelloWorld.java
    Removed Print statement

--- JenkinsHelloWorld/HelloWorld.java
+++ JenkinsHelloWorld/HelloWorld.java
@@ -3,6 +3,5 @@ package JenkinsHelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to Jenkins and Git integration");
-        System.out.println("Welcome to Java world");
     }
 }
