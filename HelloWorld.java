ae97f062f7b444ad9f1fde97c4a38968660ae8ea
816437 <jaga.joghee@gmail.com>
2022-07-18 15:54:33 +0530

    Commit HelloWorld Program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package JenkinsHelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java program with Jenkins Training");
+    }
+}
