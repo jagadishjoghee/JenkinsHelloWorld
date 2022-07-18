0674f9860d44209701164dcaea77050f1d03e972
816437 <jaga.joghee@gmail.com>
2022-07-18 17:45:15 +0530

    Added print statement

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package JenkinsHelloWorld;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Jenkins and Git integration");
-    }
-}
---
+++ JenkinsHelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+package JenkinsHelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Jenkins and Git integration");
+        System.out.println("Welcome to Java world");
+    }
+}
